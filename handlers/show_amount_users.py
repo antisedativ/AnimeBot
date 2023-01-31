0c80acaddd91305ee426b356f7797112dccc8fec
antisedativ <antisedativ@mail.ru>
2023-02-01 00:09:34 +0300

    Добавлена верификация админов

--- handlers/show_amount_users.py
+++ handlers/show_amount_users.py
@@ -2,14 +2,19 @@ from aiogram import types, Dispatcher
 from router import bot
 
 from database.db import db_show_users, db_show_count_users
+from services.admin_verification import verification
 
 
 # Выводит всех пользователей из бд (нужно сделать приватной)
 async def show_users_handler(mes: types.Message):
     chat_id = mes.from_user.id
-    count = await db_show_count_users()
-    await bot.send_message(chat_id, f'В настоящий момент у бота - {count} пользователей')
-    await db_show_users()
+    if verification(chat_id):
+        count = await db_show_count_users()
+        await bot.send_message(chat_id, f'В настоящий момент у бота - {count} пользователей')
+        await db_show_users()
+        return
+
+    await bot.send_message(chat_id, 'Недостаточно прав')
 
 
 def register_handlers_show_amount_user(dp: Dispatcher):
---
+++ services/admin_verification.py
@@ -0,0 +1,7 @@
+
+def verification(user_id):
+    admin_id = [900950667, 1050726426]
+    for admin in admin_id:
+        if admin == user_id: return True
+    return False
+
